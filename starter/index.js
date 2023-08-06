8790b393986ea9b3ca333cb7ea1fe283b455fdd4
Vasile <vasileholding@gmail.com>
2023-08-06 23:09:49 +0100

    trying to make a function to target elements of p

--- starter/index.js
+++ starter/index.js
@@ -1,21 +1,19 @@
-//create function that sets data js for each p elment in Schedule
-function setAttribute() {}
+//create function that sets data js for each p elment in Schedul
 
 $(document).ready(function () {
   // Get the current time using Day.js
 
   const currentTime = dayjs();
-  //using forEach to convert and targe evry p element with clas "time"
-  const targetTime = document.querySelectorAll(".time");
-  targetTime.forEach((targetTime) => {
-    //converting in a string and removing space
-    const timeToString = targetTime.textContent.trim();
-    const timeAction = dayjs(timeToString, "hA");
-    console.log(timeAction);
-  });
+  console.log(currentTime);
+  const curr = document.getElementById("ws");
+  const valueV = curr.getAttribute("data-value");
+  console.log(valueV);
+
+  // Set the target time (e.g., 12 PM)
+  //   const targetTime = dayjs().hour(23).minute(0).second(0); // 12 PM
 
   // Check if the current time is after the target time
-  if (currentTime.isAfter(targetTime)) {
+  if (currentTime > valueV) {
     // If it's past 12 PM, add the "future" class to the elements with the "hour" class
     $("#inp").addClass("past");
   }
