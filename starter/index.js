d47e9a991d1d944430f8fb9ef0e9d1482895679d
Vasile <vasileholding@gmail.com>
2023-08-06 22:23:54 +0100

    adding function to target each p element

--- starter/index.js
+++ starter/index.js
@@ -1,12 +1,22 @@
+//create function that sets data js for each p elment in Schedule
+function setAttribute() {}
+
 $(document).ready(function () {
   // Get the current time using Day.js
-  const currentTime = dayjs();
 
-  // Set the target time (e.g., 12 PM)
-  const targetTime = dayjs().hour().minute().second(); // 12 PM
+  const currentTime = dayjs();
+  //using forEach to convert and targe evry p element with clas "time"
+  const targetTime = document.querySelectorAll(".time");
+  targetTime.forEach((targetTime) => {
+    //converting in a string and removing space
+    const timeToString = targetTime.textContent.trim();
+    const timeAction = dayjs(timeToString, "hA");
+    console.log(timeAction);
+  });
 
   // Check if the current time is after the target time
   if (currentTime.isAfter(targetTime)) {
+    // If it's past 12 PM, add the "future" class to the elements with the "hour" class
     $("#inp").addClass("past");
-  } else
+  }
 });
