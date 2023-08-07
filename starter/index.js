61aa5d1c2f2f551c841a90a23658f57175e96fda
Vasile <vasileholding@gmail.com>
2023-08-07 12:41:13 +0100

    had abug when targeting the html elements , used container as this for iterating and each loopwas iterating just once, fixed to row class and it iterates through each row class and works

--- starter/index.js
+++ starter/index.js
@@ -10,21 +10,24 @@ function checkTime() {
   console.log(currentTime);
   //   const valueV = $(".number").data("value");
   //   console.log(valueV);
+
   //eaterating over data value inside conainer to compare the data value with the time and sett the class acordinly to the time
-  $(".container").each(function () {
+  $(".row").each(function () {
     var TimeT = parseInt($(this).find(".number").attr("data-value")); //targeting id inside container and prase it into a number
     // TimeT = TimeT.split("-")[0];
     // var newT = parseInt(TimeT);
+
     console.log(TimeT);
     var colorChange = $(this).find(".form-control");
     if (TimeT === currentTime) {
       colorChange.addClass("present");
     } else if (TimeT < currentTime) {
-      colorChange.addClass("future");
-    } else {
       colorChange.addClass("past");
+    } else {
+      colorChange.addClass("future");
     }
-    return TimeT;
+
+    // return TimeT;
     // console.log(newT);
   });
 }
