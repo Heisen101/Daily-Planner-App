fe361482d6f6952bb9f7848f20ea4eaa5c952025
Vasile <vasileholding@gmail.com>
2023-08-07 12:21:16 +0100

    trying to make it work, but it doesnt

--- starter/index.js
+++ starter/index.js
@@ -11,19 +11,21 @@ function checkTime() {
   //   const valueV = $(".number").data("value");
   //   console.log(valueV);
   //eaterating over data value inside conainer to compare the data value with the time and sett the class acordinly to the time
-
   $(".container").each(function () {
-    var TimeT = $(this).find(".number").attr("id");
-    TimeT = dayjs(TimeT, "HH");
+    var TimeT = parseInt($(this).find(".number").attr("data-value")); //targeting id inside container and prase it into a number
+    // TimeT = TimeT.split("-")[0];
+    // var newT = parseInt(TimeT);
+    console.log(TimeT);
     var colorChange = $(this).find(".form-control");
     if (TimeT === currentTime) {
-      colorChange.addClass("present").css("background-color", "#adf7d1");
-    } else if (currentTime < TimeT) {
+      colorChange.addClass("present");
+    } else if (TimeT < currentTime) {
       colorChange.addClass("future");
     } else {
       colorChange.addClass("past");
     }
-    console.log(TimeT);
+    return TimeT;
+    // console.log(newT);
   });
 }
 checkTime();
